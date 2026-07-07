9ce32fc2382cf323182e1e20d060af4b32267cdc
AlexandrGorbunov564 <irinagorbunova564@gmail.com>
2026-07-07 23:28:00 +0300

    Сделал задание 5

---
+++ Task 5/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email, password, profiledescription) VALUES ('marsel', 'sidikov@gmail.com', 'qwerty007', 'Programmer');
+INSERT INTO users (name, email, password, profiledescription) VALUES ('roman', 'roman@gmail.com', 'qwerty008', 'Student');
